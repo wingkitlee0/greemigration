7f0000fd337eab5dee6466b9f6c98a4392b6f37c
Kit Lee <wklee4993@gmail.com>
2019-10-01 10:50:36 -0400

    minor update on the UI

--- webapp4/views.py
+++ webapp4/views.py
@@ -88,10 +88,21 @@ def main():
             y_axis_label='Approval Ratio')
         plot.xaxis.axis_label_text_font_size = "20pt"
         plot.yaxis.axis_label_text_font_size = "20pt"
+        plot.xaxis.axis_label_text_font_style = "normal"
+        plot.yaxis.axis_label_text_font_style = "normal"
+
+        # vertical line below the median dot
+        plot.line([t50, t50], [-100, 0.5], line_width=3, line_dash='dashed', line_color='black')
+        # main curve
         plot.line(times, 1.0-sur_rate, line_width=5)
+        # median dot
         plot.circle(t50, 0.5, legend="median", fill_color="white", size=12)
+        
         plot.x_range=Range1d(0, 2000)
+        plot.y_range=Range1d(0, 1.05)
         plot.legend.location = "bottom_right"
+        from bokeh.models import NumeralTickFormatter
+        plot.yaxis.formatter = NumeralTickFormatter(format='0 %')
         plot_script, plot_div = components(plot)
         templateData['plot_script'] = plot_script
         templateData['plot_div'] = plot_div
